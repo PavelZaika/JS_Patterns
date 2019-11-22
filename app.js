0160a030811648ba828080c105629d7d3b26cdb7
Loki <zpk.loki@gmail.com>
2019-11-22 16:18:16 +0300

    Singleton

--- app.js
+++ app.js
@@ -31,15 +31,14 @@ const UICtrl = (function() {
 
 UICtrl.callChangeText();
 
-
 //REVEALING MODULE PATTERN
 
-const ItemCtrl = (function () {
+const ItemCtrl = (function() {
   let data = [];
 
   function add(item) {
     data.push(item);
-    console.log('Item Added.....')
+    console.log("Item Added.....");
   }
 
   function get(id) {
@@ -51,8 +50,35 @@ const ItemCtrl = (function () {
   return {
     add: add,
     get: get
+  };
+})();
+
+ItemCtrl.add({ id: 1, name: "Alex" });
+console.log(ItemCtrl.get(1));
+
+
+
+
+// SINGLETON
+
+const Singleton = (function () {
+  let instance;
+
+  function createInstance() {
+    const object = new Object({name:'Max'});
+    return object;
+  }
+
+  return{
+    getInstance: function() {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
   }
 })();
 
-ItemCtrl.add({id: 1, name: 'Alex'});
-console.log(ItemCtrl.get(1));
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
+console.log(instanceA === instanceB);
